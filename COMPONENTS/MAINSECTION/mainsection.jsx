c5e59a592e2019b7d360771d8573d815e336b22c
NIKHIL K <114824383+NIKHIL-KARAMTHODI@users.noreply.github.com>
2026-06-10 10:35:59 +0530

    Add MainSection component with page routing

---
+++ COMPONENTS/MAINSECTION/mainsection.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import './mainsection.css';
+import Trending from '../../COMPONENTS/TRENDING/trending';
+import PopularArtist from '../../COMPONENTS/POPULARARTISTS/popularartist';
+import SpecificMusicFile from '../../PAGES/SPECIFIC_MUSIC_PAGE/specificmusicfile';
+
+
+const MainSection = () => {
+  const [currentPage, setCurrentPage] = useState("home");
+  const [currentFile, setCurrentFile] = useState(null);
+
+  console.log(currentPage);
+
+  return (
+    <main className="mainSection">
+      {currentPage === "home" ? (
+        <>
+          <Trending setCurrentFile={setCurrentFile} setCurrentPage={setCurrentPage} />
+          <PopularArtist setCurrentPage={setCurrentPage} />
+        </>
+      ) : currentPage === "musicSpecificPage" ? (
+        <>
+        <SpecificMusicFile currentEle={currentFile} />
+        </>
+      ) : currentPage === "artistSpecificPage" ? (
+        <>artist</>
+      ) : null}
+    </main>
+  );
+};
+
+export default MainSection;
