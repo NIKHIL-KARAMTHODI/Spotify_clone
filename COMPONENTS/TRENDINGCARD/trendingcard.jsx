d995782e7f5b11c0b11ba1a8dac97d038863d3c2
NIKHIL K <114824383+NIKHIL-KARAMTHODI@users.noreply.github.com>
2026-06-10 10:40:28 +0530

    Add TrendingCard component with data mapping

---
+++ COMPONENTS/TRENDINGCARD/trendingcard.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './trendingcard.css';
+import { TrendingCardData } from '../../CONFIG/config';
+
+const TrendingCard = ({ setCurrentPage, setCurrentFile }) => {
+  return (
+    <div className="TrendingCardMainContainer">
+      {TrendingCardData.map((ele, id) => (
+        <div
+          key={id}
+          onClick={() => {
+            setCurrentPage("musicSpecificPage");
+            setCurrentFile(ele);
+          }}
+          className="CardContainer"
+        >
+          <img src={ele.imgSrc} alt={ele.heading} />
+          <div className="CardTextContainer">
+            <h2>{ele.heading}</h2>
+            <p>{ele.subHeading}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default TrendingCard
