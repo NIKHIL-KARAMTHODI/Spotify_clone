e03714bd420e9385bc5fea1fb0344076de225da9
NIKHIL K <114824383+NIKHIL-KARAMTHODI@users.noreply.github.com>
2026-06-10 10:53:24 +0530

    Add TrendingCardData and ArtistList to config.js

---
+++ config.js
@@ -0,0 +1,48 @@
+import Img1 from './Img1.jpg'
+import Img2 from './Img2.jpg'
+import Img3 from './Img3.jpg'
+import Img4 from './Img4.jpg'
+import Img5 from './Img5.jpg'
+import Img6 from './Img6.jpg'
+import Artist1 from './Artist1.jpg'
+import Artist2 from './Artist2.jpg'
+import Artist3 from './Artist3.jpg'
+import Artist4 from './Artist4.jpg'
+import Artist5 from './Artist5.jpg'
+import Artist6 from './Artist6.jpg'
+
+export const TrendingCardData = [
+        {imgSrc : Img1,
+         heading : "Revenge Of Raga - DC",
+         subHeading : "Anirudh Ravichander"
+        },
+        {imgSrc : Img2,
+         heading : "God Mode - Karuppu",
+         subHeading : "Sai Abhyankkar"
+        },
+        {imgSrc : Img3,
+         heading : "Kadha Thudarum - Thudarum",
+         subHeading : "Jakes Bejoy"
+        },
+        {imgSrc : Img4,
+         heading : "Muzhumathi - Jodha Akbar",
+         subHeading : "A R Rahman"
+        },
+        {imgSrc : Img5,
+         heading : "Malarae - Premam",
+         subHeading : "Rajesh Murugesan"
+        },
+        {imgSrc : Img6,
+         heading : "Shine - Album",
+         subHeading : "Hellen Slater"
+        }
+            ]
+            
+export const ArtistList = [
+    {img: Artist1, name:"A R Rahman"},
+    {img: Artist2, name:"Amith Thrivedi"},
+    {img: Artist3, name:"Shreya Ghosal"},
+    {img: Artist4, name:"Harris Jayaraj"},
+    {img: Artist5, name:"Anirudh Ravichander"},
+    {img: Artist6, name:"Jakes Bejoy"},
+]
